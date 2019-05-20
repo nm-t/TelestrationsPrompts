5d57531c8910e7e974dad8ce40fd19a0ebddadc6
Nathalia Tan <natan@deloitte.com.au>
2019-05-20 22:04:38 +1000

    [FEAT] Added random() and reset() functions. MVP!

--- main.js
+++ main.js
@@ -1,13 +1,66 @@
-function random() {
-    var url = "https://www.w3.org/TR/PNG/iso_8859-1.txt";
+let wordList = [];
+
+$(document).ready(
+    async function() {
+        await init();
+        await test();
+    }
+);
+
+async function init() {
+    var url = "https://raw.githubusercontent.com/nm-t/TelestrationsPrompts/master/list.txt";
 
     var jsonFile = new XMLHttpRequest();
-        jsonFile.open("GET",url,true);
+        jsonFile.open("GET", url, true);
         jsonFile.send();
 
     jsonFile.onreadystatechange = function() {
-        if (jsonFile.readyState== 4 && jsonFile.status == 200) {
-            document.getElementById("text-holder").innerHTML = jsonFile.responseText;
+        if (jsonFile.readyState == 4 && jsonFile.status == 200) {
+            // Start reading the file contents
+            let file = jsonFile.responseText;
+            wordList = file.split('\n');
+            console.log(wordList);
+
+            let counterHolder = [];
+            for (let j = 0; j < wordList.length; j = j + 1) {
+                counterHolder[j] = 0;
+            }
+
+            for (let i = 0; i < 1000; i = i + 1) {
+
+                let randomNumber = Math.floor(Math.random() * wordList.length);
+
+                counterHolder[randomNumber] = counterHolder[randomNumber] + 1;
+            }
+            console.log({counterHolder});
         }
     }
+}
+
+async function test() {
+    let counterHolder = [];
+    for (let j = 0; j < wordList.length; j = j + 1) {
+        counterHolder[j] = '0';
+    }
+    console.log(wordList.length);
+
+    for (let i = 0; i < 10; i = i + 1) {
+
+        let randomNumber = Math.floor(Math.random() * wordList.length);
+
+        counterHolder[randomNumber] = counterHolder[randomNumber] + 1;
+
+        // console.log({randomNumber});
+        // console.log({counterHolder});
+    }
+}
+
+function random() {
+    let randomIndex = Math.floor(Math.random() * wordList.length);
+    let randomWord = wordList[randomIndex];
+    $('#text-holder').text(randomWord);
+}
+
+function reset() {
+    $('#text-holder').text('Prompt goes here');
 }
